649ae73fc7087f73a7d192e3e76959f1b5ef7672
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-20 08:46:12 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Malcolm"
+    end
+
+    get '/hometown' do
+        'My hometown is Virginia Beach, VA.'
+    end
+
+    get '/favorite-song' do
+        "My favorite song is <em>'My Stress'</em> by NF."
+    end
 end
